d4f002231b8aa1116f2a659d24f9aed504282d8c
Sneha <snehagumber19507@gmail.com>
2025-03-28 18:46:32 +0530

    Solution to Bubble Sort for Character Array

--- Bubble Sort for Character Array@@67c9a8aa6e98bd154cc82f3f/code.c
+++ Bubble Sort for Character Array@@67c9a8aa6e98bd154cc82f3f/code.c
@@ -1,21 +1,57 @@
-int bubbleSort(char arr[],int n ){
-    char temp;int swapped=0;
-    for(int i=0;i<n;i++){
+// int bubbleSort(char arr[],int n ){
+//     char temp;int swapped=0;
+//     for(int i=0;i<n;i++){
         
-        if(arr[i]>arr[i+1]){
-            temp=arr[i];
-            arr[i]=arr[i+1];
-            arr[i+1]=temp;
-            swapped=1;
-        }
-    if(swapped==0){
-        break;
-    }
-    }
+//         if(arr[i]>arr[i+1]){
+//             temp=arr[i];
+//             arr[i]=arr[i+1];
+//             arr[i+1]=temp;
+//             swapped=1;
+//         }
+//     if(swapped==0){
+//         break;
+//     }
+//     }
     
-}
-void printArray(char arr[],int n){
-    for(int i=0;i<n;i++){
-        printf("%c ",arr[i]);
+// }
+// void printArray(char arr[],int n){
+//     for(int i=0;i<n;i++){
+//         printf("%c ",arr[i]);
+//     }
+// }
+
+#include <stdio.h>
+
+void bubbleSort(char arr[], int n) {
+    char temp;
+    
+    // Loop over each element in the array
+    for (int i = 0; i < n - 1; i++) {
+        // Flag to check if any swap was made in this iteration
+        int swapped = 0;
+        
+        // Loop through the array up to the n-i-1 element (after each iteration, the largest element is placed at the end)
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap the characters
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;  // Mark that a swap was made
+            }
+        }
+        
+        // If no swap was made, the array is already sorted, so break early
+        if (swapped == 0) {
+            break;
+        }
     }
 }
+
+void printArray(char arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%c ", arr[i]);
+    }
+    printf("\n");
+}
+
